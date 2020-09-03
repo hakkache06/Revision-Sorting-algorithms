d8e9115d22628ab51cea5af20ae0a1bdbeefcff1
1337 <hakkacheyassin@gmail.com>
2020-09-03 16:38:40 +0100

    BubbleSort

--- BubbleSort.c
+++ BubbleSort.c
@@ -15,8 +15,14 @@ void    ft_swap(int *a, int *b)
   *b = swap;
 }
 
-void ft_selectionSort(int arr[], int n)
+void ft_BubbleSort(int arr[], int n)
 {
+
+int i, j;
+   for (i = 0; i < n-1; i++)
+       for (j = 0; j < n-i-1; j++)
+           if (arr[j] > arr[j+1])
+             ft_swap(&arr[j], &arr[j+1]);
 
 }
 
@@ -24,7 +30,7 @@ int main(){
 
 int arr[] = {12,23,45,56,67,98,1};
 int n = sizeof(arr)/sizeof(arr[0]);
-
-
+ft_BubbleSort(arr,n);
+ft_printarr(arr,n);
 }
 
